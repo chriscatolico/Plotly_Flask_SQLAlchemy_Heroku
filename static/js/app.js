77d392b6ef9cc019d205ce026b0e43d65b1fc441
chriscatolico <47269593+chriscatolico@users.noreply.github.com>
2019-06-05 00:46:52 -0700

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -17,11 +17,75 @@ function buildMetadata(sample) {
     Object.entries(data).forEach(function ([key, value]) {
       panel.append("h6").text(`${key}: ${value}`);
     })
+  
+    
+    //Bonus Gauge Chart
+    var level = data.WFREQ;
+
+    // Trig to calc meter point
+    var degrees = 180 - (level*17),
+         radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+         pathX = String(x),
+         space = ' ',
+         pathY = String(y),
+         pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data3 = [{ type: 'scatter',
+       x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'wash',
+        text: level,
+        hoverinfo: 'text+name'},
+      { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+      rotation: 90,
+      text: ['10-11','8-9','6-7','4-5', '2-3', '0-1'],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                             'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                             'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                             'rgba(255, 255, 255, 0)']},
+      labels: ['10-11','8-9','6-7','4-5', '2-3', '0-1'],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+
+    var layout2 = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: 'Belly Button Washing Frequency',
+      height: 450,
+      width: 450,
+      xaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data3, layout2);
+
   })
 }
                             
 function buildCharts(sample) {
 
+    
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   var charts = "/samples/" + sample;
   
@@ -43,12 +107,12 @@ function buildCharts(sample) {
   
     var layout = {
       showlegend: false,
-      height: 500,
+      height: 650,
       width: 1000
     };
 
-    Plotly.newPlot('bubble', data1, layout);
-
+    Plotly.newPlot('bubble', data1, layout);     
+  
   //  @TODO: Build a Pie Chart
   // HINT: You will need to use slice() to grab the top 10 sample_values,
   // otu_ids, and labels (10 each).
@@ -61,8 +125,8 @@ function buildCharts(sample) {
     }];
   
     var layout1 = {
-      height: 500,
-      width: 800
+      height: 450,
+      width: 450
     };
   
     Plotly.newPlot('pie', data2, layout1);
@@ -98,3 +162,4 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
+
